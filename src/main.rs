5633c21c3e0ea96d1ed8d385c9eb3aee5b4c39a2
Prince Muel <vansomecsam@gmail.com>
2024-07-05 00:43:37 +0100

    feat: extract args parser
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::{env, fs};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    let query = args.get(1).unwrap();
-    let file_path = args.get(2).unwrap();
+    let (query, file_path) = parse_config(&args);
 
     print!("Searching for `{query}`");
     print!(" in file `{file_path}`");
@@ -12,4 +11,11 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!(" with text:\n{content}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = args.get(1).unwrap();
+    let file_path = args.get(2).unwrap();
+
+    (query, file_path)
 }
