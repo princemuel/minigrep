763aba3e3de99bf5bcbbbd5077cabe97d536c4ad
Prince Muel <vansomecsam@gmail.com>
2024-07-05 00:47:12 +0100

    feat: group config values
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ use std::{env, fs};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    print!("Searching for `{query}`");
-    print!(" in file `{file_path}`");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let content = fs::read_to_string(file_path)
+    let content = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!(" with text:\n{content}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
     let query = args.get(1).unwrap();
     let file_path = args.get(2).unwrap();
 
-    (query, file_path)
+    Config { query: query.to_string(), file_path: file_path.to_string() }
 }
