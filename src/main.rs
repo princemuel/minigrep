4d0f922ecf83782ff36885384e9ce4c74f8ec4f1
Prince Muel <vansomecsam@gmail.com>
2024-07-05 00:53:22 +0100

    feat: add a constructor for Config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, fs};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -18,9 +18,11 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args.get(1).unwrap();
-    let file_path = args.get(2).unwrap();
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let query = args.get(1).unwrap().to_string();
+        let file_path = args.get(2).unwrap().to_string();
 
-    Config { query: query.to_string(), file_path: file_path.to_string() }
+        Config { query, file_path }
+    }
 }
