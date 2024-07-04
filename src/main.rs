393f110aa55476d496c8cccf8c82bb4c36927582
Prince Muel <vansomecsam@gmail.com>
2024-07-04 03:57:01 +0100

    feat: accept command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    let query = args.get(1).unwrap();
+    let file_path = args.get(2).unwrap();
+
+    print!("Searching for {query} ");
+    print!("in file {file_path}");
 }
