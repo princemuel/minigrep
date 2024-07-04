1aef5993b392920b597854bf4b08ec4522dc50e8
Prince Muel <vansomecsam@gmail.com>
2024-07-04 04:08:15 +0100

    feat: add read file functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     let query = args.get(1).unwrap();
     let file_path = args.get(2).unwrap();
 
-    print!("Searching for {query} ");
-    print!("in file {file_path}");
+    print!("Searching for `{query}`");
+    print!(" in file `{file_path}`");
+
+    let content = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!(" with text:\n{content}");
 }
