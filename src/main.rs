1133f86b69e1f9803170ff63c8cf197d8eaaa61f
Prince Muel <vansomecsam@gmail.com>
2024-07-05 01:17:52 +0100

    refactor: move functionality to lib
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::error::Error;
-use std::{env, fs, process};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
@@ -13,32 +14,8 @@ fn main() {
         config.query, config.file_path
     );
 
-    if let Err(exception) = run(config) {
+    if let Err(exception) = minigrep::run(config) {
         println!("Application Error: {exception}");
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.file_path)?;
-    println!(" with text:\n{content}");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args.get(1).unwrap().to_string();
-        let file_path = args.get(2).unwrap().to_string();
-
-        Ok(Config { query, file_path })
-    }
 }
