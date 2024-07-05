45f2c8cff6ca6d42acccdbd197b4b794ad19595b
Prince Muel <vansomecsam@gmail.com>
2024-07-05 01:30:54 +0100

    feat: add search function
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,37 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    println!(" with text:\n{content}");
+    for line in search(&config.query, &content) {
+        println!("{line}");
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.
+";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
 }
