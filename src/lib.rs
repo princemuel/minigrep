d01a6db1821bd3b25ac4c1953f98d54e5400f304
Prince Muel <vansomecsam@gmail.com>
2024-07-05 01:41:28 +0100

    feat: implement case insensitive search
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -15,15 +16,25 @@ impl Config {
         let query = args.get(1).unwrap().to_string();
         let file_path = args.get(2).unwrap().to_string();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    for line in search(&config.query, &content) {
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search(&config.query, &content)
+    };
+
+    for line in results {
         println!("{line}");
     }
+
     Ok(())
 }
 
